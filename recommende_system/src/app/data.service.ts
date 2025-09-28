9d85e2f082b6a39b1fc188d1e16c6eaf39c8583e
nikolaossarris <nikolaossarrisnode@gmail.com>
2025-09-28 13:46:27 +0300

    third

--- recommende_system/src/app/data.service.ts
+++ recommende_system/src/app/data.service.ts
@@ -88,8 +88,20 @@ export class DataService {
       }));
   }
 
-  sendContactMessage(data: any){
-    return this.http.post<any>(this.remoteDataService.serviceURL + 'sendContactMessage', data, httpOptions).pipe(
+  getRecommendedMovie(data: any){
+    return this.http.post<any>(this.remoteDataService.serviceURL + 'recommendations/movies', data, httpOptions).pipe(
+      map(
+        (response: any) => {
+          return response;
+        }
+      ),
+      catchError((error: any) => {
+        this.handleError(error);
+        return throwError(error); // Rethrow the error to be handled by the caller
+      }));
+  }
+  getRecommendedBook(data: any){
+    return this.http.post<any>(this.remoteDataService.serviceURL + 'recommendations/books', data, httpOptions).pipe(
       map(
         (response: any) => {
           return response;
@@ -101,241 +113,8 @@ export class DataService {
       }));
   }
 
-  //For admin-------------
-  addMovie(data: any){
-    return this.http.post<any>(this.remoteDataService.serviceURL + 'addMovie', data, httpOptions).pipe(
-      map(
-        (response: any) => {
-          return response;
-        }
-      ),
-      catchError((error: any) => {
-        this.handleError(error);
-        return throwError(error); // Rethrow the error to be handled by the caller
-      }));
-  }
-
-  //For admin------------
-  updateMovie(data: any){
-    return this.http.post<any>(this.remoteDataService.serviceURL + 'updateMovie', data, httpOptions).pipe(
-      map(
-        (response: any) => {
-          return response;
-        }
-      ),
-      catchError((error: any) => {
-        this.handleError(error);
-        return throwError(error); // Rethrow the error to be handled by the caller
-      }));
-  }
-
-  //For admin-------------
-  deleteMovie(data: any){
-    return this.http.post<any>(this.remoteDataService.serviceURL + 'deleteMovie', data, httpOptions).pipe(
-      map(
-        (response: any) => {
-          return response;
-        }
-      ),
-      catchError((error: any) => {
-        this.handleError(error);
-        return throwError(error); // Rethrow the error to be handled by the caller
-      }));
-  }
-
-  getRoom(data: any){
-    return this.http.post<any>(this.remoteDataService.serviceURL + 'getRoom', data, httpOptions).pipe(
-      map(
-        (response: any) => {
-          return response;
-        }
-      ),
-      catchError((error: any) => {
-        this.handleError(error);
-        return throwError(error); // Rethrow the error to be handled by the caller
-      }));
-  }
-
-  //For Admin ------------
-  addRoom(data: any){
-    return this.http.post<any>(this.remoteDataService.serviceURL + 'addRoom', data, httpOptions).pipe(
-      map(
-        (response: any) => {
-          return response;
-        }
-      ),
-      catchError((error: any) => {
-        this.handleError(error);
-        return throwError(error); // Rethrow the error to be handled by the caller
-      }));
-  }
-
-  //For Admin ------------
-  updateRoom(data: any){
-    return this.http.post<any>(this.remoteDataService.serviceURL + 'updateRoom', data, httpOptions).pipe(
-      map(
-        (response: any) => {
-          return response;
-        }
-      ),
-      catchError((error: any) => {
-        this.handleError(error);
-        return throwError(error); // Rethrow the error to be handled by the caller
-      }));
-  }
-
-  //For Admin ------------
-  deleteRoom(data: any){
-    return this.http.post<any>(this.remoteDataService.serviceURL + 'deleteRoom', data, httpOptions).pipe(
-      map(
-        (response: any) => {
-          return response;
-        }
-      ),
-      catchError((error: any) => {
-        this.handleError(error);
-        return throwError(error); // Rethrow the error to be handled by the caller
-      }));
-  }
-
-  getSeat(data: any){
-    return this.http.post<any>(this.remoteDataService.serviceURL + 'getSeat', data, httpOptions).pipe(
-      map(
-        (response: any) => {
-          return response;
-        }
-      ),
-      catchError((error: any) => {
-        this.handleError(error);
-        return throwError(error); // Rethrow the error to be handled by the caller
-      }));
-  }
-
-  //For Admin ------------
-  addSeat(data: any){
-    return this.http.post<any>(this.remoteDataService.serviceURL + 'addSeat', data, httpOptions).pipe(
-      map(
-        (response: any) => {
-          return response;
-        }
-      ),
-      catchError((error: any) => {
-        this.handleError(error);
-        return throwError(error); // Rethrow the error to be handled by the caller
-      }));
-  }
-
-  //For Admin ------------
-  addAllSeats(data: any){
-    return this.http.post<any>(this.remoteDataService.serviceURL + 'addAllSeats', data, httpOptions).pipe(
-      map(
-        (response: any) => {
-          return response;
-        }
-      ),
-      catchError((error: any) => {
-        this.handleError(error);
-        return throwError(error); // Rethrow the error to be handled by the caller
-      }));
-  }
-
-  updateSeat(data: any){
-    return this.http.post<any>(this.remoteDataService.serviceURL + 'updateSeat', data, httpOptions).pipe(
-      map(
-        (response: any) => {
-          return response;
-        }
-      ),
-      catchError((error: any) => {
-        this.handleError(error);
-        return throwError(error); // Rethrow the error to be handled by the caller
-      }));
-  }
-
-  updateAllSeat(data: any){
-    return this.http.post<any>(this.remoteDataService.serviceURL + 'updateAllSeat', data, httpOptions).pipe(
-      map(
-        (response: any) => {
-          return response;
-        }
-      ),
-      catchError((error: any) => {
-        this.handleError(error);
-        return throwError(error); // Rethrow the error to be handled by the caller
-      }));
-  }
-
-  sendTicketToUser(data: any){
-    return this.http.post<any>(this.remoteDataService.serviceURL + 'sendTicketToUser', data, httpOptions).pipe(
-      map(
-        (response: any) => {
-          return response;
-        }
-      ),
-      catchError((error: any) => {
-        this.handleError(error);
-        return throwError(error); // Rethrow the error to be handled by the caller
-      }));
-  }
-
-  //For Admin -----------
-  deleteSeat(data: any){
-    return this.http.post<any>(this.remoteDataService.serviceURL + 'deleteSeat', data, httpOptions).pipe(
-      map(
-        (response: any) => {
-          return response;
-        }
-      ),
-      catchError((error: any) => {
-        this.handleError(error);
-        return throwError(error); // Rethrow the error to be handled by the caller
-      }));
-  }
-
-  getView(data: any){
-    return this.http.post<any>(this.remoteDataService.serviceURL + 'getView', data, httpOptions).pipe(
-      map(
-        (response: any) => {
-          return response;
-        }
-      ),
-      catchError((error: any) => {
-        this.handleError(error);
-        return throwError(error); // Rethrow the error to be handled by the caller
-      }));
-  }
-
-  // For Admin---
-  addView(data: any){
-    return this.http.post<any>(this.remoteDataService.serviceURL + 'addView', data, httpOptions).pipe(
-      map(
-        (response: any) => {
-          return response;
-        }
-      ),
-      catchError((error: any) => {
-        this.handleError(error);
-        return throwError(error); // Rethrow the error to be handled by the caller
-      }));
-  }
-
-  // For Admin---
-  updateView(data: any){
-    return this.http.post<any>(this.remoteDataService.serviceURL + 'updateView', data, httpOptions).pipe(
-      map(
-        (response: any) => {
-          return response;
-        }
-      ),
-      catchError((error: any) => {
-        this.handleError(error);
-        return throwError(error); // Rethrow the error to be handled by the caller
-      }));
-  }
-
-  // For Admin---
-  deleteView(data: any){
-    return this.http.post<any>(this.remoteDataService.serviceURL + 'deleteView', data, httpOptions).pipe(
+  getRecommendedMusic(data: any){
+    return this.http.post<any>(this.remoteDataService.serviceURL + 'recommendations/music', data, httpOptions).pipe(
       map(
         (response: any) => {
           return response;
--- recommende_system/src/app/navbar/home/home.component.ts
+++ recommende_system/src/app/navbar/home/home.component.ts
@@ -1,12 +1,70 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {AuthenticationService} from '../../auth.service';
+import {NgForOf, NgIf, NgFor} from '@angular/common';
+import {DataService} from '../../data.service';
+import {Rating} from 'primeng/rating';
+import {FormsModule} from '@angular/forms';
+import {ProgressSpinner} from 'primeng/progressspinner';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf,
+    NgForOf,
+    Rating,
+    FormsModule,
+    ProgressSpinner
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  currentUser: string = "";
+  movies: any[] = [];
+  books: any[] = [];
+  music: any[] = [];
+  moviesLoading = false;
+  booksLoading = false;
+  musicLoading = false;
+
+
+  auth = inject(AuthenticationService);
+  data = inject(DataService);
+
+  ngOnInit() {
+    this.currentUser = this.auth.currentUser()?.username;
+
+    this.getAllMovies();
+    this.getAllBooks();
+    this.getAllMusic();
+  }
+
+  getAllMovies() {
+    this.moviesLoading = true;
+
+    this.data.getRecommendedMovie({type : "War", limit: 10}).subscribe((movies: any) => {
+      this.movies = movies;
+      this.moviesLoading = false;
+    });
+  }
+
+  getAllBooks() {
+    this.booksLoading = true;
+
+    this.data.getRecommendedBook({type : "War", limit: 10}).subscribe((books: any) => {
+      this.books = books;
+      this.booksLoading = false;
+    })
+  }
+
+  getAllMusic(){
+    this.musicLoading = true;
+
+    this.data.getRecommendedMusic({type : "War", limit: 10}).subscribe((musics: any) => {
+      this.music = musics;
+      this.musicLoading = false;
+    })
+  }
 }
